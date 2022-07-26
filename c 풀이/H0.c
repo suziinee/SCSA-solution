36507eb54ea4162c10241061dd5066d7dc995590
DESKTOP-TPDLR5H\user <jsj032097@gmail.com>
2022-07-26 17:47:21 +0900

    Modified - int 배열 이용한 풀이 (포인터 사용)

--- c 풀이/H0.c
+++ c 풀이/H0.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
-void func(int n)
+int idx = 0;
+
+int* func(int n, int nums[])
 {
 	while (n) {
-		printf("%d", n % 10);
-		n /= 10;
+		nums[idx] = n % 10;
+		n /= 10; idx++;
 	}
+	return nums;
 }
 
 void main(void)
 {
 	int n;
 	scanf("%d", &n);
-	func(n);
+	int nums[100] = { 0 };
+	int* ans = func(n, nums);
+
+	for (int i = 0; i < idx; i++) {
+		printf("%d", ans[i]);
+	}
 }
 		
     
