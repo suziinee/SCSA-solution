68994b22462c5d2a4e8494bf0be8aa53f5d9b8a3
DESKTOP-TPDLR5H\user <jsj032097@gmail.com>
2022-07-27 10:10:40 +0900

    두 수를 입력받아서 그 사이에 존재하는 소수를 인쇄 - 소수를 구할때 제곱근을 이용하는 풀이 - min, max와 소수 구하는 함수를 중첩 for문으로 하나의 함수에서 처리

--- c 풀이/F4.c
+++ c 풀이/F4.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
 
-int* func(int n, int nums[])
+void func(int min, int max)
 {
-	for (int i = 2; i*i <= n; i++) {
-		if (nums[i]) {
-			int j = 2;
-			while (i*j <= n) {
-				nums[i*j] = 0;
-				j += 1; 
+	for (int i = min; i <= max; i++) {
+		int flag = 1;
+		for (int j = 2; j*j <= i; j++) {
+			if (i%j == 0) {
+				flag = 0; break;
 			}
 		}
+		if (flag) printf("%d\n", i);
 	}
-	return nums;
 }
 
 void main(void)
 {
-	int a, b; int min, max;
+	int a, b;
 	scanf("%d %d", &a, &b);
-	if ((a <= 0) || (b <= 0)) printf("Error!!");
+
+	if ((a <= 0) || (b <= 0)) {
+		printf("Error!!\n");
+	}
 	else {
-		if (a > b) {
-			min = b; max = a;
-		}
-		else {
-			min = a; max = b;
-		}
-
-		int arr[10000] = { 0 }; int arr2[10000] = { 0 };
-		for (int i = 1; i <= min; i++) arr[i] = i;
-		for (int j = 1; j <= max; j++) arr2[j] = j;
-		int* res = func(min, arr);
-		int* res2 = func(max, arr2);
-
-		printf("%d\n", 1);
-
-		for (int i = 2; i <= max; i++) {
-			if (res[i] != res2[i]) printf("%d\n", res2[i]);
-		}
+		a > b ? func(b, a) : func(a, b);
 	}
 }
