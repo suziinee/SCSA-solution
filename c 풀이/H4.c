1d68ceba1ac7a61a20cba5b22890ad4e49fa2245
DESKTOP-TPDLR5H\user <jsj032097@gmail.com>
2022-07-27 15:23:53 +0900

    폭탄 돌리기 - 배열을 사용하지 않은 간단한 풀이

--- c 풀이/H4.c
+++ c 풀이/H4.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 
-int func(int start, int n, int p_time[], char* tpf)
+void func(int k, int n)
 {
-    int bomb=start;
-    int time=0;
+	int time = 0;
+	int bomb = k;
+	int t; 
+	char a='A';
 
-    for (int i=start; i<start+n; i++) {
-        time+=p_time[i];
-        p_time[i]=0;
-        if (time<210) {
-            if (tpf[i]=='T') bomb+=1;
-        }
-        else return (bomb%8)==0 ? 8 : bomb%8;
-    }
-    return (bomb%8)==0 ? 8 : bomb%8;
+	for (int i = 0; i < n; i++) {
+		scanf(" %d %c", &t, &a);
+		time += t;
+		if (time >= 210) break;
+		else {
+			a == 'T' ? (bomb += 1) : (bomb += 0) ;
+		}
+	}
+	printf("%d", bomb % 8 == 0 ? 8 : bomb % 8);
 }
 
 void main(void)
 {
-    int p_time[1000]={0}; char tpf[1000] = {'A'};
+	int k;
+	scanf("%d", &k);
+	int n;
+	scanf(" %d", &n);
 
-    int start;
-    scanf("%d", &start);
-    int n;
-    scanf(" %d", &n);
-
-    for (int i=0; i<n; i++) {
-        scanf(" %d %s", &p_time[start+i], &tpf[start+i]);
-    }
-    printf("%d", func(start, n, p_time, tpf));
-
+	func(k, n);
 }
