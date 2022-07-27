fe4da08e162535e7946e17859d4eb1020d4422d0
DESKTOP-TPDLR5H\user <jsj032097@gmail.com>
2022-07-27 14:25:24 +0900

    폭탄 돌리기 - time 계산과 bomb을 가진 사람만으로 간단하게 구현

--- python 풀이/H4.py
+++ python 풀이/H4.py
@@ -1,26 +1,18 @@
-from collections import deque
 start = int(input())
 n = int(input())
-time_dct = {i : deque([]) for i in range(1, 9)}
-tpf_dct = {i : deque([]) for i in range(1, 9)}
-idx = [i%8 if i%8!=0 else 8 for i in range(start, n+start)]
-
-for i in idx :
-  a, b = input().split()
-  time_dct[i].append(int(a))
-  tpf_dct[i].append(b)
-
 time = 0
 bomb = start
 
-for i in idx :
-  if time_dct[i] :
-    tmp = time_dct[i].popleft()
-    if time + tmp < 210 :
-      time += tmp
-      if tpf_dct[i].popleft() == 't' :
-        bomb += 1
-  else :
+for i in range(n) :
+  t, tpf = input().split()
+  t = int(t)
+  time += t
+  if time >= 210 : 
     break
-
-print(bomb)
+  else :
+    if tpf == 'T' : bomb += 1
+  
+if bomb%8==0 : 
+  print(8)
+else :
+  print(bomb%8)
