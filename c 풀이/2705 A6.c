8f82ccc467664125f19931ba17475eff306bca12
DESKTOP-TPDLR5H\user <jsj032097@gmail.com>
2022-07-28 12:32:00 +0900

    20개 정수 정렬 - define 사용 - for문 중첩이 아니라 함수별로 쪼개서 구현 -> for문 중첩보다 swap이 덜 사용됨

--- c 풀이/2705 A6.c
+++ c 풀이/2705 A6.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
+#define ASIZE(arr) sizeof(arr)/sizeof((arr)[0])
 int a[20];
 
-void sort()
+void swap(int x, int y)
 {
 	int tmp;
-	for (int i = 0; i < 20; i++) {
-		for (int j = i + 1; j < 20; j++) {
-			if (a[i] > a[j]) {
-				tmp = a[j];
-				a[j] = a[i];
-				a[i] = tmp;
-			}
-		}
+	tmp = a[y];
+	a[y] = a[x];
+	a[x] = tmp;
+}
+
+void func(int start, int end)
+{
+	int min_idx = start;
+	for (int i = start+1; i <= end; i++) {
+		if (a[i] < a[min_idx]) min_idx = i;
 	}
+	swap(min_idx, start);
 }
 
 void main(void)
 {
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < ASIZE(a); i++) {
 		scanf("%d", &a[i]);
 	}
-
-	sort();
-	
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < ASIZE(a) - 1; i++) {
+		func(i, ASIZE(a) - 1);
+	}
+	for (int i = 0; i < ASIZE(a); i++) {
 		printf("%d ", a[i]);
 	}
 }
