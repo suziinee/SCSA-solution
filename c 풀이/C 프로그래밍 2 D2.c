a9cc9332f7b7aa24e32bbcef98c83b2b68b88c34
DESKTOP-TPDLR5H\user <jsj032097@gmail.com>
2022-08-02 17:49:28 +0900

    수열 - LIS를 한번만 돌려서 up, down을 모두 찾음 - up -> down, down -> up으로 바뀔 경우에는 끊고 max 갱신, 카운트 변수 초기화 - 마지막은 증가인지 감소인지 모르기 때문에 max, up, down 비교를 한번씩 해주기

--- c 풀이/C 프로그래밍 2 D2.c
+++ c 풀이/C 프로그래밍 2 D2.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
 
-int n;
+int N;
 int arr[100000];
 
 int LIS(void)
 {
-	int cnt = 1; int max = 1;
-	for (int i = 1; i < n; i++) {
-		if (arr[i - 1] <= arr[i]) cnt += 1;
-		else {
-			max = (max > cnt) ? max : cnt;
-			cnt = 1;
-		}
-	}
-	max = (max > cnt) ? max : cnt;
-	return max;
-}
+	int up = 1; int down = 1;
+	int max = 1;
 
-int rev_LIS(void)
-{
-	int cnt = 1; int max = 1;
-	for (int i = 1; i < n; i++) {
-		if (arr[i - 1] >= arr[i]) cnt += 1;
-		else {
-			max = (max > cnt) ? max : cnt;
-			cnt = 1;
+	for (int i = 0; i < N-1; i++) {
+		if (arr[i] <= arr[i + 1]) up += 1;
+		else { 
+			max = max > up ? max : up;
+			up = 1;
+		}
+
+		if (arr[i] >= arr[i + 1]) down += 1;
+		else { 
+			max = max > down ? max : down;
+			down = 1;
 		}
 	}
-	max = (max > cnt) ? max : cnt;
+	max = max > down ? max : down;
+	max = max > up ? max : up;
 	return max;
 }
 
 void main(void)
 {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
 		scanf("%d", &arr[i]);
+	}
 	int lis = LIS();
-	int rev_lis = rev_LIS();
-	printf("%d", lis > rev_lis ? lis : rev_lis);
+	printf("%d", lis);
 }
