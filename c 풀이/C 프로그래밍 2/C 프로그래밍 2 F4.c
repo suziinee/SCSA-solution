488afbaeb92a4695fa360c2e4fedb5ac4751dd4f
suziinee <susan3722@snu.ac.kr>
2022-08-04 14:55:27 +0900

    2개의 문자열을 교환하는 Swap 함수 - null cnt가 2개면 2개의 문자열 모두 끝까지 도달한 것이므로 swap 종료

--- c 풀이/C 프로그래밍 2/C 프로그래밍 2 F4.c
+++ c 풀이/C 프로그래밍 2/C 프로그래밍 2 F4.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 
-char tmp;
-
-void copy(char* before, char* after, int start)
-{
-	for (start; before[start] != '\0'; start++) {
-		after[start] = before[start];
-	}
-	after[start] = before[start];
-}
-
 void Swap(char* a, char* b)
 {
 	int null_cnt = 0;
-	int i;
+	char tmp;
 
-	for (i = 0; ; i++) {
-		tmp = b[i];
-		b[i] = a[i];
-		a[i] = tmp;
-		if (a[i] == '\0' || b[i] == '\0') break;
-	}
-	if (a[i] == '\0' && b[i] == '\0') return;
-	else if (a[i] == '\0') { //b가 더 짦음
-		copy(a, b, i + 1);
-	}
-	else if (b[i] == '\0') { //a가 더 짧음
-		copy(b, a, i + 1);
+	while (null_cnt < 2) {
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+		if (!*a) null_cnt++;
+		if (!*b) null_cnt++;
+		a++; b++;
 	}
 }
 
+int main(void)
+{
+	char a[110], b[110];
+	scanf("%s %s", a, b);
+	Swap(a, b);
+	printf("%s %s\n", a, b);
+	return 0;
+}
 void main(void)
 {
 	char a[110], b[110];
