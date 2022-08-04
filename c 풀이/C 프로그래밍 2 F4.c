936ba9732384424de6b179752f60ce6144032fc6
suziinee <susan3722@snu.ac.kr>
2022-08-04 14:33:54 +0900

    2개의 문자열을 교환하는 Swap 함수 - 문자열이 끝나는 지점인 null의 개수가 2개가 되면 2개의 문자 모두 끝까지 도달한 것이므로 종료

---
+++ c 풀이/C 프로그래밍 2 F4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void Swap(char* a, char* b)
+{
+	int null_cnt = 0;
+	char tmp;
+
+	while (null_cnt < 2) {
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
+		if (!*a) null_cnt++;
+		if (!*b) null_cnt++;
+		a++; b++;
+	}
+}
+
+int main(void)
+{
+	char a[110], b[110];
+	scanf("%s %s", a, b);
+	Swap(a, b);
+	printf("%s %s\n", a, b);
+	return 0;
+}
