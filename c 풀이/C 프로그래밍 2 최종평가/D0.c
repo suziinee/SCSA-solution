1fb1f0b11493fc57f8205e091223dac7e2fba5ee
suziinee <susan3722@snu.ac.kr>
2022-08-11 12:45:04 +0900

    순환 순열 - 일단 버퍼를 10000개 정도 채워 넣음 - 시작지점이 0~4999번 인덱스까지 가능하고 따라서 반복 개수는 1~5000개까지 가능함 - 각 시작지점에 대해 반복개수 5000개까지 돌림 (바깥 for문이 시작지점) - 시작지점 start에 대해 반복개수 n보다 작을 경우 while문을 돌려서 비교함     - 이때 코너케이스인 시작지점 1, 반복개수 1일 경우 while문을 돌지 않으므로 조건에 넣어줌 - 반복 후 flag가 바뀌지 않으면 리턴, flag가 바뀌면 다시 그 다음 반복개수를 돈다 - 결함 : 정답 개수가 5000개 이하라고 가정해서 푼 방법

--- c 풀이/C 프로그래밍 2 최종평가/D0.c
+++ c 풀이/C 프로그래밍 2 최종평가/D0.c
@@ -2,63 +2,36 @@
 
 int n;
 int p;
-int arr[1000000];
-int i = 2;
+int arr[10000 + 10];
+
+void put(void)
+{
+	arr[0] = n;
+	for (int i = 1; i < 10000; i++) {
+		arr[i] = arr[i - 1] * n % p;
+	}
+}
 
 int func(void)
-{
-	arr[0] = n; arr[1] = n * n % p; arr[2] = (arr[1] * n) % p;
-	for (;;) {
-		int tmp = (arr[i] * n) % p;
-		if ((i + 1) % 2 == 0) { //i가 1,3,5..
-			int j = (i + 1) / 2; int flag = 0;
-			if (arr[j] == arr[0]) {
-				printf("0 if\n");
-				for (int k = 0; k <= (i + 1) / 2 - 1; k++, j++) {
-					if (arr[k] != arr[j]) {
-						flag = 1; break;
-					}
+{	
+	for (int i = 0; i < 5000; i++) { //시작지점
+		for (int n = 1; n < 5000; n++) { //반복개수
+			int flag = 0; int start = i;
+			while ((start == 1 && n==1)||(start < n)) {
+				if (arr[start] != arr[start + n]) {
+					flag = 1; break;
 				}
-				if (flag == 0) return i / 2;
+				start++;
 			}
-			else if (arr[j] == arr[1]) {
-				printf("1 else\n");
-				for (int k = 1; k <= (i + 1) / 2; k++, j++) {
-					if (arr[k] != arr[j + 1]) {
-						flag = 1; break;
-					}
-				}
-				if (flag == 0) return i / 2;
-			}
+			if (flag == 0) return n;
 		}
-
-		else { //i가 0,2,4...
-			int j = (i / 2) + 1; int flag = 0;
-			if (arr[j] == arr[0]) {
-				for (int k = 0; k <= (i / 2); k++, j++) {
-					if (arr[k] != arr[j]) {
-						flag = 1; break;
-					}
-				}
-				if (flag == 0) return (i / 2) + 1;
-			}
-			else if (arr[j] == arr[1]) {
-				for (int k = 1; k <= (i / 2); k++, j++) {
-					if (arr[k] != arr[j]) {
-						flag = 1; break;
-					}
-				}
-				if (flag == 0) return (i / 2);
-			}
-		}
-		arr[i + 1] = tmp;
-		i++;
-	}
+	}	
 }
 
 int main(void)
 {
 	scanf("%d %d", &n, &p);
+	put();
 	int ans = func();
 	printf("%d", ans);
 	return 0;
