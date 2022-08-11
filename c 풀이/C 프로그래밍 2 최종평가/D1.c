b220eba0eab77c409b9511df4db0652f20b5bded
suziinee <susan3722@snu.ac.kr>
2022-08-11 17:19:38 +0900

    행복한 수 - happy한 수는 그 이전 수열도 모두 happy하고 unhappy도 규칙이 같으므로 해시를 만들어서 숫자를 저장해둠 - func의 메인 for문에서 애초에 happy나 unhappy에 걸리면 is_happy 함수 실행을 하지 않음 - is_happy 함수 내부에서도 pow를 구할때마다 tmp가 happy나 unhappy에 들어있는지 확인     - 각각에 들어있다면 그동안 저장한 arr를 모두 happy나 unhappy에 넣음

--- c 풀이/C 프로그래밍 2 최종평가/D1.c
+++ c 풀이/C 프로그래밍 2 최종평가/D1.c
@@ -2,6 +2,8 @@
 
 int n;
 int max = 1;
+int happy[100000];
+int unhappy[100000];
 
 int pow(int num)
 {
@@ -21,14 +23,24 @@ int in_arr(int *arr, int num)
 	return 0;
 }
 
-int is_happy(int num) //행복한 수 1 리턴
+int is_happy(int num) //happy[i]==0인 수만 들어옴
 {
 	int arr[1000] = { 0, };
 	int i = 0; arr[0] = num;
 	for (i = 0; ; i++) {
 		int tmp = pow(arr[i]);
-		if (tmp == 1) return 1;
-		if (in_arr(arr, tmp)) return 0;
+		if (happy[tmp] || (tmp == 1)) { //arr를 모두 happy에 넣음
+			for (int j = 0; j <= i + 1; j++) {
+				if (happy[arr[j]] == 0) happy[arr[j]] = 1;
+			}
+			return 1;
+		}
+		if (unhappy[tmp] || (in_arr(arr, tmp))) { //arr를 모두 unhappy에 넣음
+			for (int j = 0; j <= i + 1; j++) {
+				if (unhappy[arr[j]] == 0) unhappy[arr[j]] = 1;
+			}
+			return 0;
+		}
 		arr[i + 1] = tmp;
 	}
 }
@@ -36,6 +48,10 @@ int is_happy(int num) //행복한 수 1 리턴
 void func(void)
 {
 	for (int i = 1; i <= n; i++) {
+		if (happy[i]) {
+			if (i > max) max = i;
+		}
+		if (unhappy[i]) continue;
 		if (is_happy(i)) {
 			if (i > max) max = i;
 		}
