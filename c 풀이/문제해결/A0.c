37149d161bbbfa6c707e311eea8399ef7cfcdcf0
suziinee <susan3722@snu.ac.kr>
2022-08-16 11:05:19 +0900

    십자카드 문제 - clock num과 nonclock num을 해시로 넣어놓고 판단 - clock이나 nonclock 배열에 없을 경우 hash_clock 함수를 돌려서 해당 숫자카드로 만들 수 있는 3개의 nonclock과 1개의 clock을 해시 배열에 넣는다

--- c 풀이/문제해결/A0.c
+++ c 풀이/문제해결/A0.c
@@ -3,19 +3,19 @@
 int num[5];
 int clock;
 int ans = 1;
+int clock_hash[10000 + 10];
+int nonclock_hash[10000 + 10];
 
-int make_clock(int num)
+void hash_clock(int num) //hash에 없는 숫자들을 hash에 넣어주는 함수
 {
 	int tmp[4] = { 0 }; int cpy = num;
 	for (int i = 3; i >= 0; i--) {
 		tmp[i] = cpy % 10;
-		if (tmp[i] == 0) return 0;
 		cpy /= 10;
 	}
 
 	int candi[4] = { 0 };
 	candi[0] = num;
-
 	for (int i = 0; i < 3; i++) {
 		candi[i + 1] = (candi[i] - tmp[i] * 1000) * 10 + tmp[i];
 	}
@@ -25,15 +25,35 @@ int make_clock(int num)
 		if (candi[i] < candi[min]) min = i;
 	}
 	
-	return candi[min];
+	for (int i = 0; i < 4; i++) {
+		if (i == min) clock_hash[candi[i]] = 1;
+		else nonclock_hash[candi[i]] = 1;
+	}
+}
+
+void make_clock(void)
+{
+	clock = 1000 * num[0] + 100 * num[1] + 10 * num[2] + num[3];
+	int candi[4] = { 0 }; candi[0] = clock;
+	for (int i = 0; i < 3; i++) {
+		candi[i + 1] = (candi[i] - num[i] * 1000) * 10 + num[i];
+	}
+	for (int i = 0; i < 4; i++) {
+		if (clock > candi[i]) clock = candi[i];
+	}
 }
 
 void func(void)
 {
-	clock = 1000 * num[0] + 100 * num[1] + 10 * num[2] + num[3];
-	clock = make_clock(clock);
+	make_clock();
 	for (int i = 1111; i < clock; i++) {
-		if (make_clock(i) == i) ans++;
+		if (clock_hash[i]) ans++;
+		else if (nonclock_hash[i]) continue;
+		else {
+			hash_clock(i);
+			if (clock_hash[i]) ans++;
+			else if (nonclock_hash[i]) continue;
+		}
 	}
 }
 
