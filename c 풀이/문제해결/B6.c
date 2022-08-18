338e919dc5bcb25e95ed88da4d7008b30f7bb6a4
suziinee <susan3722@snu.ac.kr>
2022-08-18 13:06:14 +0900

    미친수열 - k를 구하는 공식을 찾아서 k를 일단 구함 - sqrt를 사용해서 doulbe로 결과가 나오는데 int로 설정해서 **내림**이 됨 - 이때 정확히 나누어 떨어지면 +1을 하면 안되므로 결과값 k를 가지고 1~k까지의 합 tmp를 만들어서 n과 비교     - n이 tmp와 같지 않으면 (tmp가 더 작음) k++

--- c 풀이/문제해결/B6.c
+++ c 풀이/문제해결/B6.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 unsigned long long int n;
 
 void func(void)
 {
-	unsigned long long int tmp = 0;
-	unsigned long long int i = 1;
-	for (;;) {
-		tmp += i;
-		if (n <= tmp) {
-			printf("%d", i);
-			return;
-		}
-		i++;
-	}
+	unsigned long long int k = (unsigned long long int)(-1 + sqrt(1 + 8 * n)) / 2;
+	unsigned long long int tmp = (k*(k + 1)) / 2;
+	if (tmp < n) k++;
+	printf("%llu", k);
 }
 
 void main(void)
