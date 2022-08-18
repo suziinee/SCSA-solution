760ab7c938c3f8b7493fd9d702b482a60c24a56c
suziinee <susan3722@snu.ac.kr>
2022-08-18 15:42:43 +0900

    색종이 만들기 - countpaper 함수에서 check, divide 모두 해줌

--- c 풀이/문제해결/C0.c
+++ c 풀이/문제해결/C0.c
@@ -2,14 +2,7 @@
 
 int n;
 int arr[150][150];
-int white; int blue;
-typedef struct st {
-	int x1; int y1; 
-	int x2; int y2;
-	int x3; int y3;
-	int x4; int y4;
-} D;
-void func(int x, int y, int L);
+int w; int b;
 
 void input(void)
 {
@@ -21,51 +14,36 @@ void input(void)
 	}
 }
 
-void check(int x, int y, int L)
+void countpaper(int y, int x, int L)
 {
+	int tmp = arr[y][x];
+
 	if (L == 1) {
-		if (arr[y][x] == 0) white++;
-		else blue++;
+		if (tmp == 0) w++;
+		else b++;
 		return;
 	}
-	else {
-		int tmp = arr[y][x];
-		for (int i = y; i < y + L; i++) {
-			for (int j = x; j < x + L; j++) {
-				if (tmp != arr[i][j]) {
-					func(x, y, L);
-					return;
-				}
+
+	for (int i = y; i < y + L; i++) {
+		for (int j = x; j < x + L; j++) {
+			if (arr[i][j] != tmp) {
+				countpaper(y, x, L / 2);
+				countpaper(y, x + L / 2, L / 2);
+				countpaper(y + L / 2, x, L / 2);
+				countpaper(y + L / 2, x + L / 2, L / 2);
+				return;
 			}
 		}
-		if (tmp == 0) white++;
-		else blue++;
 	}
-}
 
-D divide(int x, int y, int L) //(y, x)로 시작해서 L개씩 4개로 쪼개줌
-{
-	D d;
-	d.x1 = x; d.y1 = y;
-	d.x2 = x + L; d.y2 = y;
-	d.x3 = x; d.y3 = y + L;
-	d.x4 = x + L; d.y4 = y + L;
-	return d;
-}
-
-void func(int x, int y, int L) //L개를 쪼개고 체크해주는 함수
-{
-	D tmp = divide(x, y, L / 2);
-	check(tmp.x1, tmp.y1, L/2);
-	check(tmp.x2, tmp.y2, L/2);
-	check(tmp.x3, tmp.y3, L/2);
-	check(tmp.x4, tmp.y4, L/2);
+	if (tmp == 0) w++;
+	else b++;
 }
 
 
 void main(void)
 {
 	input();
-	check(1, 1, n);
-	printf("%d\n%d", white, blue);
+	countpaper(1, 1, n);
+	printf("%d\n%d", w, b);
 }
