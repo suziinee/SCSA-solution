92e1ef25a6c99110635296c43207ac54821a1daf
suziinee <susan3722@snu.ac.kr>
2022-08-31 00:29:35 +0900

    고기잡이 - 1만개 X 1만개 배열 -> segmentation fault => map을 형성하지 않고 풀어야함 - 물고기가 있는 점들만 확인     - 각 좌표마다 상하좌우 좌표에 대해서, 그 좌표에 대해 4가지 그물방향으로 탐색

--- c 풀이/문제해결2/A5.cpp
+++ c 풀이/문제해결2/A5.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 using namespace std;
 
-#define MAX 10000
 int n, t, m;
-int map[MAX + 10][MAX + 10];
 int ans;
+struct AXIS {
+	int y; int x;
+};
+vector<AXIS> fish;
 
 void input()
 {
 	cin >> n >> t >> m;
 	for (int i = 0; i < m; i++) {
-		int y, x;
-		cin >> y >> x;
-		map[y][x] = 1;
+		AXIS a;
+		cin >> a.y >> a.x;
+		fish.push_back(a);
 	}
 }
 
@@ -22,25 +24,59 @@ void output()
 	cout << ans;
 }
 
+int net(const AXIS& a, const AXIS& b, const AXIS& c, const AXIS& d)
+{
+	//범위체크
+	if (a.x<1 || a.y<1 || a.x>n || a.y>n) return 0;
+	if (b.x<1 || b.y<1 || b.x>n || b.y>n) return 0;
+	if (c.x<1 || c.y<1 || c.x>n || c.y>n) return 0;
+	if (d.x<1 || d.y<1 || d.x>n || d.y>n) return 0;
+
+	//안에 물고기가 몇마리 있는지
+	int sum = 0;
+	for (int f = 0; f < m; f++) {
+		if (fish[f].x >= a.x && fish[f].x <= c.x && fish[f].y >= a.y && fish[f].y <= c.y) {
+			sum += 1;
+		}
+	}
+	return sum;
+}
+
+
+const int dx[] = { 0, 0, 1, 0, -1 };
+const int dy[] = { 0, -1, 0, 1, 0 };
 void check(int* res)
 {
-	for (int y = 1; y <= n - res[0]; y++) {
-		int tmp = 0; //해당 y행에서 먹을 수 있는 물고기
-		for (int x = 1; x <= n - res[1]; x++) {
-			if (x == 1) { //처음에는 초기화
-				for (int i = y; i <= y + res[0]; i++) {
-					for (int j = 1; j <= 1 + res[1]; j++) {
-						if (map[i][j] == 1) tmp++;
-					}
-				}
-			}
-			else { //나머지는 sliding windows
-				for (int i = y; i <= y + res[0]; i++) {
-					if (map[i][x - 1] == 1) tmp--;
-					if (map[i][x + res[1]] == 1) tmp++;
-				}
-			}
-			if (ans < tmp) ans = tmp; //매번 update
+	int a = res[0]; int b = res[1];
+
+	for (int i = 0; i < m; i++) {
+
+		for (int dir = 0; dir < 5; dir++) {
+			int nx = fish[i].x + dx[dir];
+			int ny = fish[i].y + dy[dir];
+
+			int sum;
+			AXIS n1; AXIS n2; AXIS n3; AXIS n4;
+
+			n1 = { ny, nx }; n2 = { ny, nx + b };
+			n3 = { ny + a, nx + b }; n4 = { ny + a, nx };
+			sum = net(n1, n2, n3, n4);
+			if (sum > ans) ans = sum;
+
+			n1 = { ny - a, nx }; n2 = { ny - a, nx + b };
+			n3 = { ny, nx + b }; n4 = { ny, nx };
+			sum = net(n1, n2, n3, n4);
+			if (sum > ans) ans = sum;
+
+			n1 = { ny - a, nx - b }; n2 = { ny - a, nx };
+			n3 = { ny, nx }; n4 = { ny, nx - b };
+			sum = net(n1, n2, n3, n4);
+			if (sum > ans) ans = sum;
+
+			n1 = { ny, nx - b }; n2 = { ny, nx };
+			n3 = { ny + a, nx }; n4 = { ny + a, nx - b };
+			sum = net(n1, n2, n3, n4);
+			if (sum > ans) ans = sum;
 		}
 	}
 }
