789bc550ebef55545c47dff7d4ed0308ae4edacb
suziinee <susan3722@snu.ac.kr>
2022-09-01 21:11:18 +0900

    나무 자르기 - 0부터 최대 나무 길이까지 중간부터 시작해서 자를 수 있는 길이인지 아닌지 판단 - 이때 판단하는 possible 함수는 O(n)이지만 나무 길이는 절반씩 줄여나가므로 O(nlogn)으로 풀 수 있음

--- c 풀이/문제해결2/C2.cpp
+++ c 풀이/문제해결2/C2.cpp
@@ -25,28 +25,34 @@ void output()
 }
 
 
+bool possible(const int& tar)
+{
+	long long sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += (tree[i] > tar ? tree[i] - tar : 0);
+		if (sum >= m) return true;
+	}
+	return false;
+}
+
+
 void solve()
 {
-	sort(tree.begin(), tree.end());
-
-	long long int sum = tree[n - 1];
-	for (int i = n - 2; i >= 0; i--) {
-		sum += tree[i];
-		int cut_tree = n - i; //잘려진 나무의 수
-		int rest = sum - (tree[i] * cut_tree); //자르고 남은 나무 길이
-		if (rest == m) { //tree[i]가 정답
-			ans = tree[i];
-			return;
-		}
-		else if (rest > m) { //tree[i]+1~tree[i+1]-1 사이에 정답이 있음
-			ans = (rest - m) / (cut_tree - 1) + tree[i];
-			return;
-		}
-		
+	int max = -1;
+	for (int i = 0; i < n; i++) {
+		if (tree[i] > max) max = tree[i];
 	}
-	if (ans == 0) { //모두 같은 크기의 나무
-		ans = tree[n - 1] - (m / n);
-		return;
+
+	int s = 0; int e = max;
+	while (s <= e) {
+		int mid = (s + e) / 2;
+		if (possible(mid)) { //높이를 더 높일 수 있음
+			ans = mid;
+			s = mid + 1;
+		}
+		else {
+			e = mid - 1;
+		}
 	}
 }
 
