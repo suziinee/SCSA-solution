770ce909b579e3305984e94a82035c580d24f362
suziinee <susan3722@snu.ac.kr>
2022-09-03 01:22:20 +0900

    마라톤 대회(Bronze) - 시험 끝나고 조금 더 정규화해서 푼 풀이

--- c 풀이/문제해결2/C7.cpp
+++ c 풀이/문제해결2/C7.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int n;
 struct AXIS { int x; int y; };
-vector<AXIS> pnt;
+vector<AXIS> chk;
 
 
 void input()
@@ -14,7 +14,7 @@ void input()
 	for (int i = 0; i < n; i++) {
 		int x, y;
 		cin >> x >> y;
-		pnt.push_back({ x, y });
+		chk.push_back({ x, y });
 	}
 }
 
@@ -29,33 +29,34 @@ int solve()
 {
 	int ans = 0;
 
-	int max = -1 * 0x7fffffff;
-	int max_idx;
+	vector<int> diff; //현재 인덱스와 바로 전 인덱스와의 차이
+	diff.push_back(0);
+	for (int i = 1; i < n; i++) {
+		diff.push_back(abs(chk[i].x - chk[i - 1].x) + abs(chk[i].y - chk[i - 1].y));
+	}
 
-	vector<int> diff; diff.push_back(0);
+	//1번째부터 n-2번째까지 해당 인덱스를 제거했을때 이득을 구함
+	int max = -1 * 0x7fffffff; int idx;
 	for (int i = 1; i < n - 1; i++) {
+		int a1 = diff[i];
+		int a2 = abs(chk[i].x - chk[i + 1].x) + abs(chk[i].y - chk[i + 1].y);
+		int b1 = abs(chk[i - 1].x - chk[i + 1].x) + abs(chk[i - 1].y - chk[i + 1].y);
 
-		int a1 = abs(pnt[i].x - pnt[i - 1].x) + abs(pnt[i].y - pnt[i - 1].y);
-		int a2 = abs(pnt[i].x - pnt[i + 1].x) + abs(pnt[i].y - pnt[i + 1].y);
-		int b1 = abs(pnt[i + 1].x - pnt[i - 1].x) + abs(pnt[i + 1].y - pnt[i - 1].y);
-		
-		diff.push_back(a1);
-		if ((a1 + a2) - (b1) > max) {
-			max_idx = i;
+		if (a1 + a2 - b1 > max) {
+			idx = i;
 			max = a1 + a2 - b1;
 		}
 	}
-	diff.push_back(abs(pnt[n - 2].x - pnt[n - 1].x) + abs(pnt[n - 2].y - pnt[n - 1].y));
 
-	//max_idx 지점의 체크포인트를 빼고 달리는 거리 계산
-	for (int i = 0; i < diff.size(); i++) {
-		if (i == max_idx) {
-			ans += abs(pnt[max_idx + 1].x - pnt[max_idx - 1].x) + abs(pnt[max_idx + 1].y - pnt[max_idx - 1].y);
+	//idx를 제외하고 거리 구함
+	for (int i = 0; i < n; i++) {
+		if (i == idx) {
+			ans += abs(chk[i - 1].x - chk[i + 1].x) + abs(chk[i - 1].y - chk[i + 1].y);
 		}
-		else if (i == max_idx + 1) continue;
+		else if (i == idx + 1) continue;
 		else ans += diff[i];
 	}
-	
+
 	return ans;
 }
 
