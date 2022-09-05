ebad6875bde1981318cc0ecebbe8fb77fd960dd2
suziinee <susan3722@snu.ac.kr>
2022-09-06 00:08:41 +0900

    공주님의 정원 - 피는 날짜의 최댓값을 저장하는 배열을 만들어서 해시로 풀이 - 실패 시에 while문 break를 하고 while문 밖에서 ans를 리턴하면 안됨!! 무조건 즉시 0을 리턴해야함 - start 날짜와 end 날짜 사이에 최대 지는 날짜인 tmp를 구해서 end와 비교 -> tmp<=end 면 실패

--- c 풀이/문제해결2/B1.cpp
+++ c 풀이/문제해결2/B1.cpp
@@ -1,72 +1,53 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
 int n;
-struct FLOWER {
-	int start; int end;
-};
-vector<FLOWER> flo;
-int ans;
-int flag = 0;
+int year[1250];
 
 
 void input()
 {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		int a, b, c, d;
-		cin >> a >> b >> c >> d;
-		flo.push_back({ a * 100 + b, c * 100 + d });
+		int sm, sd, em, ed;
+		cin >> sm >> sd >> em >> ed;
+		if (year[sm * 100 + sd] < (em * 100 + ed)) year[sm * 100 + sd] = em * 100 + ed;
 	}
 }
 
-
-void output()
+int solve()
 {
-	if (flag == 1) cout << 0;
-	else cout << ans;
-}
-
-
-bool compare(const FLOWER& f1, const FLOWER& f2)
-{
-	return f1.start < f2.start;
-}
-
-
-void solve()
-{
-	sort(flo.begin(), flo.end(), compare);
+	int ans = 0;
 	
-	int start = 3 * 100 + 1;
-	int i = 0;
-
+	int s = 1 * 100 + 1;
+	int e = 3 * 100 + 1; 
+	
 	while (true) {
-		int maxend = 0;
-		//day날까지 피는 날 중에 가장 늦게 지는 꽃 선택
-		while (i < n && flo[i].start <= start) {
-			if (flo[i].end > maxend) maxend = flo[i].end;
-			i++;
+		int tmp = 0;
+		for (int i = s; i <= e; i++) {
+			if (year[i] > tmp) tmp = year[i];
 		}
 
-		//실패 체크*** => 고른 maxend가 시작 날짜보다 작으면 이어지지 않음 + 굳이 i==n 체크 안해도됨
-		if (maxend < start) { flag = 1; return; }
-		//실패가 아니라면 꽃 개수 증가
+		//실패 체크
+		if (e >= tmp) return 0; //tmp는 지는 날 하루 이후이므로 e==tmp여도 이어지지 않음
+		
 		ans++;
-		//성공 체크
-		if (maxend > 1130) return;
-		//start 갱신
-		start = maxend;
+		if (tmp > 1130) return ans; //11/30까지 꽃이 피어있어야함
+		s = e + 1;
+		e = tmp;
 	}
+}
+
+void output(const int& ans)
+{
+	cout << ans;
 }
 
 
 int main()
 {
 	input();
-	solve();
-	output();
+	int ans = solve();
+	output(ans);
 	return 0;
 }
