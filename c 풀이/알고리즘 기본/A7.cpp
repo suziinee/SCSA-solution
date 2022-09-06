26726bb4d907224a47bc036851a29c255cca01d8
suziinee <susan3722@snu.ac.kr>
2022-09-06 19:12:32 +0900

    자동차경주대회-SPJ - data의 바로 뒤 정비소부터 n+1 정비소까지 탐색하는 방법을 psum 배열 사용하지 않고 누적 sum을 이용해 while문 사용

--- c 풀이/알고리즘 기본/A7.cpp
+++ c 풀이/알고리즘 기본/A7.cpp
@@ -14,7 +14,6 @@ queue<STN> q;
 int chk[MAXN + 2]; //n+1번이 end, 해당 상태까지 최소 방문 시간
 int path[MAXN + 2];
 vector<STN> stn;
-int psum[MAXN + 2]; 
 
 
 void input()
@@ -22,13 +21,10 @@ void input()
 	cin >> max_dist >> n;
 
 	//0번 정비소 (시작지점)
-	int sum = 0;
 	for (int i = 0; i <= n; i++) {
 		int a;
 		cin >> a;
 		stn.push_back({ i, a, 0 }); //val은 비워두기
-		sum += a;
-		psum[i + 1] = sum;
 	}
 	for (int i = 1; i <= n; i++) {
 		int a;
@@ -41,22 +37,24 @@ void input()
 int bfs()
 {
 	fill(chk, chk + n + 2, 0x7fffffff); //0번부터 n+1번까지 사용 예정
-	q.push({ stn[0] }); //{0, _, 0}
+	q.push({ stn[0] }); 
 	chk[0] = 0;
 
 	while (!q.empty()) {
 		STN data = q.front(); q.pop();
 
 		//바로 뒤 정비소부터(data.n + 1) n+1 정비소까지 max_dist 내에 있는 정비소들 탐색
-		for (int i = data.n + 1; i <= n + 1; i++) {
-			if (psum[i] - psum[data.n] > max_dist) break;
-
+		int i = data.n + 1;
+		int sum_dist = data.b_dist;
+		while (i <= (n + 1) && sum_dist <= max_dist) {
 			STN ndata = stn[i];
 			if (chk[ndata.n] > chk[data.n] + ndata.val) {
 				chk[ndata.n] = chk[data.n] + ndata.val;
 				path[ndata.n] = data.n;
 				q.push(ndata);
 			}
+			i++;
+			sum_dist += ndata.b_dist;
 		}
 	}
 
