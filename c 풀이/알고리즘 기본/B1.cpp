75ec404f4cadd01439423c8f16d22e4aa901929a
suziinee <susan3722@snu.ac.kr>
2022-09-07 17:01:55 +0900

    더하기 - dfs에서 옳은 결과를 찾으면 true를 리턴하고 옳지 않으면 false를 리턴하는 방식 - 중간에 K보다 sum이 커지는 경우 가지치기 추가

--- c 풀이/알고리즘 기본/B1.cpp
+++ c 풀이/알고리즘 기본/B1.cpp
@@ -5,24 +5,25 @@ using namespace std;
 
 int T, N, K;
 vector<int> nums;
-bool flag;
 
 
-void dfs(int n, int sum)
+bool dfs(int n, int sum) //dfs를 행하던 중 만족하지 않으면 false를 리턴
 {
-	if (sum == K) { flag = true; return; }
-	if (n == N) return;
+	if (sum == K) return true;
+	if (sum > K) return false;
+	if (n == N) return false;
 
 	//n번째 포함
-	dfs(n + 1, sum + nums[n]);
+	if (dfs(n + 1, sum + nums[n])) return true;
 	//n번째 미포함
-	dfs(n + 1, sum);
+	if (dfs(n + 1, sum)) return true;
+	//둘 다 true가 아닐 경우
+	return false;
 }
 
 void solve()
 {
-	dfs(0, 0);
-	if (flag) cout << "YES" << "\n";
+	if (dfs(0, 0)) cout << "YES" << "\n";
 	else cout << "NO" << "\n";
 }
 
@@ -32,7 +33,6 @@ void input()
 	for (int t = 0; t < T; t++) {
 		//초기화
 		nums = {};
-		flag = 0;
 
 		cin >> N >> K;
 		for (int i = 0; i < N; i++) {
