3bded0401b4098ba3e79a845b846c96f11606eda
suziinee <susan3722@snu.ac.kr>
2022-09-07 17:35:01 +0900

    건물세우기(BASIC2) - 장소를 pick에 뽑아놓고 n개가 완성되면 pick에 있는 장소들 순서대로 sum을 구하는 방법 - dfs가 끝나면 chk 배열에서 체크를 해제해주는 것 잊지 않기

---
+++ c 풀이/알고리즘 기본/B3.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAXN 10
+int N;
+int arr[MAXN + 5][MAXN + 5];
+int chk[MAXN + 5];
+int pick[MAXN + 5];
+int ans = 0x7fffffff;
+
+
+void input()
+{
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> arr[i][j];
+		}
+	}
+}
+
+void dfs(int n) //depth만 파라미터로 가짐
+{
+	if (n > N) {
+		int sum = 0; 
+		//chk배열에서 1인 곳의 장소인 pick 배열에서 순서대로 꺼내기
+		for (int i = 1; i <= N; i++) {
+			sum += arr[i][pick[i]];
+		}
+		if (sum < ans) ans = sum;
+		return;
+	}
+
+	for (int i = 1; i <= N; i++) { //장소
+		if (chk[i] == 0) {
+			chk[i] = 1; //chk에 체크하고
+			pick[n] = i; //pick에 넣어주기
+			dfs(n + 1);
+			chk[i] = 0;
+		}
+	}
+}
+
+void solve()
+{
+	dfs(1);
+	cout << ans;
+}
+
+
+int main(void)
+{
+	input();
+	solve();
+	return 0;
+}
