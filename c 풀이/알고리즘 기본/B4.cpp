bfba8c2d48ce945c4281c4db3072651a35177af9
suziinee <susan3722@snu.ac.kr>
2022-09-07 17:39:12 +0900

    건물세우기(BASIC2)-SPJ - pick을 사용하지 않고 sum 파라미터를 사용한 방법

--- c 풀이/알고리즘 기본/B4.cpp
+++ c 풀이/알고리즘 기본/B4.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int N;
 int arr[MAXN + 5][MAXN + 5];
 int chk[MAXN + 5];
-int pick[MAXN + 5];
 int ans = 0x7fffffff;
 int route[MAXN + 5];
+int ans_route[MAXN + 5];
 
 
 void input()
@@ -21,37 +21,37 @@ void input()
 	}
 }
 
-void dfs(int n) //depth만 파라미터로 가짐
+void dfs(int n, int sum) //depth, sum
 {
 	if (n > N) {
-		int sum = 0; 
-		//chk배열에서 1인 곳의 장소인 pick 배열에서 순서대로 꺼내기
-		for (int i = 1; i <= N; i++) {
-			sum += arr[i][pick[i]];
-		}
 		if (sum < ans) {
 			ans = sum;
-			copy(pick, pick + N + 1, route);
+			copy(route, route + N + 1, ans_route);
 		}
 		return;
 	}
 
 	for (int i = 1; i <= N; i++) { //장소
 		if (chk[i] == 0) {
-			chk[i] = 1; //chk에 체크하고
-			pick[n] = i; //pick에 넣어주기
-			dfs(n + 1);
+			chk[i] = 1; //n번째 알파벳의 i번째 장소를 선택할 것
+			route[n] = i;
+			sum += arr[n][i];
+
+			dfs(n + 1, sum);
+
 			chk[i] = 0;
+			sum -= arr[n][i];
+			route[n] = 0;
 		}
 	}
 }
 
 void solve()
 {
-	dfs(1);
+	dfs(1, 0);
 	cout << ans << "\n";
 	for (int i = 1; i <= N; i++) {
-		cout << route[i] << " ";
+		cout << ans_route[i] << " ";
 	}
 }
 
