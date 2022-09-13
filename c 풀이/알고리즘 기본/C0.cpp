639513d7d829370d0518a9d0cf97cd62d3550589
suziinee <susan3722@snu.ac.kr>
2022-09-13 14:59:12 +0900

    보너스 - 시험 끝나고 다시 풀어봄

--- c 풀이/알고리즘 기본/C0.cpp
+++ c 풀이/알고리즘 기본/C0.cpp
@@ -1,55 +1,52 @@
 #include <iostream>
 #include <algorithm>
-#include <list>
 using namespace std;
 
 int N, M;
-list<int> rel[10 + 1];
-int chk[10 + 1];
-int pick[10 + 1];
-int money[10 + 1];
+struct CONN { int high; int low; };
+vector<CONN> conn;
+vector<int> money;
+int pick[10];
+int chk[10];
 
 
 void input()
 {
 	cin >> N >> M;
-	for (int i = 1; i <= M; i++) {
-		int a, b;
-		cin >> a >> b;
-		rel[a].push_back(b);
+	for (int i = 0; i < M; i++) {
+		int high, low;
+		cin >> high >> low;
+		conn.push_back({ high, low });
 	}
-	for (int i = 1; i <= N; i++) {
-		cin >> money[i];
+	for (int i = 0; i < N; i++) {
+		int m;
+		cin >> m;
+		money.push_back(m);
 	}
 }
 
-bool pick_ok() //pick[2]부터 pick[N]까지 조건을 만족하는지 판단
+bool check()
 {
-	for (int i = 2; i <= N; i++) {
-		for (int r : rel[i]) {
-			if (pick[i] <= pick[r]) return false;
-		}
+	for (CONN c : conn) {
+		if (pick[c.high - 1] <= pick[c.low - 1]) return false;
 	}
 	return true;
 }
 
 bool dfs(int n)
 {
-	if (n > N) {
-		if (pick_ok()) return true;
+	if (n >= N) {
+		if (check()) return true;
 		return false;
 	}
 
-	for (int i = 2; i <= N; i++) {
-		if (chk[i] == 0) {
-			pick[n] = money[i];
-			chk[i] = 1;
-			if (dfs(n + 1)) {
-				chk[i] = 0;
-				return true;
-			}
-			chk[i] = 0;
-		}
+	for (int i = 1; i < N; i++) {
+		if (chk[i]) continue;
+
+		pick[n] = money[i];
+		chk[i] = 1;
+		if (dfs(n + 1)) return true;
+		chk[i] = 0;
 	}
 	return false;
 }
@@ -58,11 +55,11 @@ bool compare(const int& i1, const int& i2) { return i1 > i2; }
 
 void solve()
 {
-	sort(money + 1, money + N + 1, compare);
-	pick[1] = money[1];
-
-	dfs(2);
-	for (int i = 1; i <= N; i++) { cout << pick[i] << " "; }
+	sort(money.begin(), money.end(), compare);
+	pick[0] = money[0];
+	if (dfs(1)) {
+		for (int i = 0; i < N; i++) cout << pick[i] << " ";
+	}
 }
 
 
