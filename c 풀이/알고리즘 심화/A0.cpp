f325820f44f3f82899693a391f5339863a2dbfab
suziinee <susan3722@snu.ac.kr>
2022-09-13 12:36:34 +0900

    N QUEEN - 좌측, 우측 상향 대각선도 flag 배열로 만들어서 판단을 쉽게 함 - 인덱스를 0~N으로 바꿈

--- c 풀이/알고리즘 심화/A0.cpp
+++ c 풀이/알고리즘 심화/A0.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 #define MAXN 13
 int N;
-int chess[MAXN + 1][MAXN + 1];
-int flag[MAXN + 1];
+int flag_col[MAXN];
+int flag_r[2 * MAXN - 1]; //flag_l[r-c+(N-1)]
+int flag_l[2 * MAXN - 1]; //flag_l[r+c]
 int ans;
 
 
@@ -13,41 +14,31 @@ void input()
 	cin >> N;
 }
 
-int check(const int& r, const int& c)
-{
-	//좌측 상향 대각선 확인
-	for (int rr = r - 1, cc = c + 1; rr >= 1 && cc <= N; rr--, cc++) {
-		if (chess[rr][cc]) return 0;
-	}
-	//우측 상향 대각선 확인
-	for (int rr = r - 1, cc = c - 1; rr >= 1 && cc >= 1; rr--, cc--) {
-		if (chess[rr][cc]) return 0;
-	}
-	return 1;
-}
-
 void dfs(int r) //모두 다른 열에 놓아질것, r행이 놓아질 열 선택
 {
-	if (r > N) {
+	if (r >= N) {
 		ans++;
 		return;
 	}
 
-	for (int c = 1; c <= N; c++) {
-		if (flag[c]) continue;
-		if (!check(r, c)) continue; //좌측 상향, 우측 상향 대각선 선택
+	for (int c = 0; c < N; c++) {
+		if (flag_col[c]) continue;
+		if (flag_r[r - c + (N - 1)]) continue;
+		if (flag_l[r + c]) continue;
 
-		flag[c] = 1;
-		chess[r][c] = 1;
+		flag_col[c] = 1;
+		flag_r[r - c + (N - 1)] = 1;
+		flag_l[r + c] = 1;
 		dfs(r + 1);
-		flag[c] = 0;
-		chess[r][c] = 0;
+		flag_col[c] = 0;
+		flag_r[r - c + (N - 1)] = 0;
+		flag_l[r + c] = 0;
 	}
 }
 
 void solve()
 {
-	dfs(1);
+	dfs(0);
 	cout << ans;
 }
 
