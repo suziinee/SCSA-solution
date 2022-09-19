c7404e0b9066e0c5d7d3b225ddbfa3cf8d312754
suziinee <susan3722@snu.ac.kr>
2022-09-19 15:34:09 +0900

    카드게임(BASIC) - chk배열을 2차원으로 만들어서 chk[l][r]일때 최대 점수를 담는 배열로 만듦 - 가지치기는 chk[l][r]이 현재 score + ri[r]보다 크거나 같으면 dfs를 수행하지 않고 리턴 - prefix_r 가지치기도 함께 사용해줌

--- c 풀이/알고리즘 심화/B5.cpp
+++ c 풀이/알고리즘 심화/B5.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int N;
 int le[MAXN + 1];
 int ri[MAXN + 1];
-int ans = 0;
-
+int chk[MAXN + 1][MAXN + 1]; //l, r일때 최대 점수를 담기
 int prefix_r[MAXN + 1];
+int ans = 0;
 
 
 void input()
@@ -24,16 +24,23 @@ void input()
 
 void dfs(int l, int r, int score)
 {
+	//가지치기
+	if (chk[l][r] >= score + ri[r]) return;
 	if (score + prefix_r[N] - prefix_r[r - 1] <= ans) return;
 
+	//종료조건
 	if (l > N || r > N) {
 		if (ans < score) ans = score;
 		return;
 	}
 
 	//오른쪽이 더 작으면 오른쪽 버리기
-	if (le[l] > ri[r]) dfs(l, r + 1, score + ri[r]);
-	else {
+	if (le[l] > ri[r] && chk[l][r] < score + ri[r]) {
+		chk[l][r] = score + ri[r];
+		dfs(l, r + 1, score + ri[r]);
+	}
+	else if (le[l] <= ri[r] && chk[l][r] < score) {
+		chk[l][r] = score;
 		//왼쪽만 버리기 
 		dfs(l + 1, r, score);
 		//왼쪽 + 오른쪽 버리기 
@@ -43,6 +50,7 @@ void dfs(int l, int r, int score)
 
 void solve()
 {
+	fill(chk[0], chk[MAXN + 1], -1);
 	dfs(0, 0, 0);
 	cout << ans;
 }
