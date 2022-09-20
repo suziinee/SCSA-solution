cd9947e3e216af82fbfb8b3b2a6187d6755fe1a5
DESKTOP-L4QE09O\jsj03 <susan3722@snu.ac.kr>
2022-09-20 11:22:23 +0900

    투표 - 후보자들의 구조체를 만들고 그 주소와 이름을 map에 전달     -> 주소를 전달하는 이유는 투표할때 이름으로 점수를 찾아야 해서 map을 사용해야 하는데, 또한 cand를 정렬해야 하기 때문에 구조체가 아닌 주소만 전달해서 내용 변경     -> cand 배열을 vector로 사용할 경우 vector에 push하면서 동시에 map에 주소를 전달하면 안됨 -> 배열과 달리 vector는 주소가 연속적이지 않기 때문에 모두 push를 한 이후에 map에 주소를 전달해야함 - cand 구조체 정렬 시 < operator를 재정의하는 방식 사용 (compare 함수 만들어도 됨)

--- c 풀이/알고리즘 심화/B7.cpp
+++ c 풀이/알고리즘 심화/B7.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <string>
 #include <algorithm>
 using namespace std;
 
 int N, M;
-struct PERSON { int score; int num; };
-unordered_map<string, PERSON> ht;
+
+struct CAND {
+	string name;
+	int idx;
+	int score;
+	bool operator<(CAND& c) {
+		if (score == c.score) return idx < c.idx;
+		return score > c.score;
+	}
+};
+vector<CAND> cand;
+unordered_map<string, CAND*> ht;
 
 
 void input()
@@ -16,36 +25,39 @@ void input()
 	string s;
 	for (int i = 0; i < N; i++) {
 		cin >> s;
-		ht.insert({ s, { 0, i } });
+		cand.push_back({ s, i, 0 });
 	}
-	cin >> M;
-	int score;
-	for (int i = 0; i < M; i++) {
-		cin >> s >> score;
-		auto iter = ht.find(s);
-		if (iter != ht.end()) (iter->second.score) += score;
+	for (int i = 0; i < N; i++) {
+		ht.insert({ cand[i].name, &cand[i] });
 	}
 }
 
-bool compare(const pair<string, PERSON>& p1, const pair<string, PERSON>& p2) {
-	if (p1.second.score == p2.second.score) return p1.second.num < p2.second.num;
-	return p1.second.score > p2.second.score;
+void vote()
+{
+	cin >> M;
+	string s; int n;
+	for (int i = 0; i < M; i++) {
+		cin >> s >> n;
+		auto iter = ht.find(s);
+		if (iter != ht.end()) {
+			iter->second->score += n;
+		}
+	}
 }
 
 void solve()
 {
-	vector<pair<string, PERSON>> vec(ht.begin(), ht.end());
-	sort(vec.begin(), vec.end(), compare);
-
+	sort(cand.begin(), cand.end());
 	for (int i = 0; i < 3; i++) {
-		cout << vec[i].first << " " << vec[i].second.score << "\n";
+		cout << cand[i].name << " " << cand[i].score << "\n";
 	}
 }
 
 
-int main(void)
+int main()
 {
 	input();
+	vote();
 	solve();
 	return 0;
 }
